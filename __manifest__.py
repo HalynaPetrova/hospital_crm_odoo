9e5ca5d248a8ac40e8b23f1ca0746e7c32696907
halyna <1208petrova@gmail.com>
2024-02-29 12:41:12 +0200

    create patient.xml file

--- __manifest__.py
+++ __manifest__.py
@@ -5,6 +5,9 @@
     "category": "Human Resources",
     "depends": [],
     "data": [
+        'security/ir.model.access.csv',
+        'views/menu_items.xml',
+        'views/patient.xml',
     ],
     "installable": True,
     "application": True,
